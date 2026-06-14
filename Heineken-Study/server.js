721adffffacbc45d6c469937896cd058ab249956
Vipin Karthikeyan <kv8896@gmail.com>
2026-06-14 14:26:30 -0700

    Update server.js

--- Heineken-Study/server.js
+++ Heineken-Study/server.js
@@ -6,9 +6,14 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log('Starting server...');
+console.log('__dirname:', __dirname);
+console.log('dist path:', path.join(__dirname, 'dist'));
+
 app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
+  console.log('Request:', req.path);
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
